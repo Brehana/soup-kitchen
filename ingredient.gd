ea44c61893c7052be141892a5858410df0d981d9
Miguel Ramos <ramosmiguel40@gmail.com>
2025-06-01 16:33:21 -0700

    Began implementing global ingredients and recipes

---
+++ ingredient.gd
@@ -0,0 +1,6 @@
+class_name Ingredient extends RigidBody2D
+
+signal ingredient_cooked(ingredient_name, ingredient_quantity)
+signal ingredient_spent(ingredient_name, ingredient_quantity)
+
+var ingredient_name: String
---
+++ src/global_ingredients_inventory.gd
@@ -0,0 +1,9 @@
+extends Node
+
+##A global script for tracking the ingredients that have been prepared through
+##minigames.
+
+##Completed ingredients and their quantities are stored as key value pairs.
+
+var ingredients_completed = {"Meat": 3,
+							"Veggies": 2}
